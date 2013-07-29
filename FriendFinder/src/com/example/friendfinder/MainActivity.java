c45d260e352d391d2542842b7f798e129c4b51e0
Tanase Sebastian <tanase.sebastian@gmail.com>
2013-07-29 13:50:28 +0200

    test from seb

--- FriendFinder/src/com/example/friendfinder/MainActivity.java
+++ FriendFinder/src/com/example/friendfinder/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
 	}
 //coucou
 	//loloooloo
+	//seb
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
