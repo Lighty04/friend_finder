a1c7286f1bb767895861daecd0d6c7267370b4b1
Martin <Martin@Rmi-pc>
2013-07-30 10:29:51 +0200

    Interface creation and User class creation

---
+++ FriendFinder/gen/com/example/friendfinder/User.java
@@ -0,0 +1,13 @@
+package com.example.friendfinder;
+
+public class User {
+	
+	
+	private String name;
+	private int id;
+	private String email;
+	private int latitude;
+	private int longitude;
+	private int meta_id;
+
+}
