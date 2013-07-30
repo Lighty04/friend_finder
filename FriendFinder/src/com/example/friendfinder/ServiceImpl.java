7f7738173f8f80127170c82b9f94a170139af091
Martin <Martin@Rmi-pc>
2013-07-30 11:36:23 +0200

    ServiceImplementation creation

---
+++ FriendFinder/src/com/example/friendfinder/ServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.friendfinder;
+
+import java.util.List;
+
+public class ServiceImpl implements ServiceInterface {
+
+	@Override
+	public boolean NewUser(User user, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Connect(User user, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean AddFriend(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean DeleteFriend(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User FindAFriend(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> FindAllFriend() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
