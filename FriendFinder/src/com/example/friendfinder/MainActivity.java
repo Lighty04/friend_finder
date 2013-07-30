0c2d25338f9c5a6aa543f27d78a1f349cddcabb2
Juan <jglivinalli@gmail.com>
2013-07-30 11:28:38 +0200

    branch

--- FriendFinder/src/com/example/friendfinder/MainActivity.java
+++ FriendFinder/src/com/example/friendfinder/MainActivity.java
@@ -12,20 +12,19 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	// Trololol
-	//coucou2
-	//lol
-	//hellodfre
-	//nice
-	//hey theregzutf
-	//kony
-	//another try
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+		
+		
 		return true;
 	}
+	
+	
+	
 
 }
 
