a1af99c339dcd75e48540edc2b3a3abaa23d7ee3
Kony404 <kony404@hotmail.com>
2013-07-30 15:13:32 +0200

    removed comments

--- FriendFinder/src/com/example/friendfinder/MainActivity.java
+++ FriendFinder/src/com/example/friendfinder/MainActivity.java
@@ -12,17 +12,6 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	// Trololol
-	//coucou2
-	//lol
-	//hellodfre
-	//nice
-	//hey theregzutf
-	//kony
-	//another try
-	
-	//test
-	//test2
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
