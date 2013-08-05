daf32f5fcb57aecebba8eca14e9e1c4f56ce15e2
Daniel <Daniel@student.aau.dk>
2013-08-06 01:19:04 +0200

    auto geoloc

--- FriendFinder/src/com/example/friendfinder/ConnectivityCheck.java
+++ FriendFinder/src/com/example/friendfinder/ConnectivityCheck.java
@@ -1,13 +1,78 @@
 package com.example.friendfinder;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-public class ConnectivityCheck {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+public class ConnectivityCheck extends FragmentActivity {
+
+	public final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+	public static class ErrorDialogFragment extends DialogFragment {
+
+		private Dialog mDialog;
+
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+			switch (resultCode) {
+			case Activity.RESULT_OK:
+				// try the request again here
+				break;
+			}
+			break;
+		}
+	}
+
+	private boolean servicesConnected() {
+		int resultCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(getApplicationContext()); // Verify
+																			// that
+																			// it
+																			// is
+																			// the
+																			// good
+																			// context
+		if (ConnectionResult.SUCCESS == resultCode) {
+			Log.d("Location updates", "Google Play sevices is available");
+			return true;
+		} else { // google play services not available :(
+			Log.d("Location updates", "There was an error. ");
+			return false;
+		}
+
+	}
 
 	// contains the functions to check if :
 	// GPS
 	// Data
+	// Google play services
+
 	public boolean checkLocalization(Context context) {
 		LocationManager lm = null;
 		boolean gps_enabled = false;
--- FriendFinder/src/com/example/friendfinder/GeolocService.java
+++ FriendFinder/src/com/example/friendfinder/GeolocService.java
@@ -2,29 +2,92 @@ package com.example.friendfinder;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
 
 public class GeolocService extends Service {
 
 	private final IBinder mBinder = new MyBinder();
-	
+	private LocationClient mLocationClient;
+	private Location mCurrentLocation;
+
+	public class GooglePlayServicesGeoloc extends FragmentActivity implements
+			GooglePlayServicesClient.ConnectionCallbacks,
+			GooglePlayServicesClient.OnConnectionFailedListener {
+
+
+		@Override
+		public void onConnectionFailed(ConnectionResult result) {
+			try {
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void onConnected(Bundle connectionHint) {
+			Toast.makeText(getApplicationContext(), "Connected",
+					Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onDisconnected() {
+			Toast.makeText(getApplicationContext(),
+					"Disconnected. Please reconnect", Toast.LENGTH_SHORT)
+					.show();
+
+		}
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			mLocationClient = new LocationClient(this, this, this);
+		}
+
+		@Override
+		protected void onStart() {
+			super.onStart();
+			// Connect the client.
+			mLocationClient.connect();
+		}
+
+		@Override
+		protected void onStop() {
+			// Disconnecting the client invalidates it.
+			mLocationClient.disconnect();
+			super.onStop();
+		}
+
+	}
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
+
 		// code here what's to be done when the service is started
 		// launch geoloc
-		
-		// When geoloc ok, send to the corresponding user in the database his position
-		
+		mCurrentLocation = mLocationClient.getLastLocation();
+
+		// When geoloc ok, send to the corresponding user in the database
+		// his
+		// position
+
 		return Service.START_NOT_STICKY;
 	}
-	
+
 	public class MyBinder extends Binder {
 		GeolocService getService() {
 			return GeolocService.this;
 		}
 	}
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
